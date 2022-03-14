eb8787bd31e1e2c159cc52772d2864f9e3e1c400
Pierre <pierre.roumane@epitech.eu>
2022-03-14 14:34:35 -0400

    <feat> added classification

--- index.js
+++ index.js
@@ -10,30 +10,39 @@ client.on('ready', () => {
 client.on('message', msg => {
 
     const string = new String(msg);
-    //console.log(msg.author.username);
     
-    tab = ['mathis', 'bite', "eternium", "ping", "meme", 'pierre', 'penis', 'vagin', 'meilleur serv', "boy"];
+    //word bank // add a word in the bank when you add a feature
+    tab = ['queen', 'nice', 'matis', 'mathis', 'bite', "eternium", "ping", "meme", 'pierre', 'penis', 'vagin', 'meilleur serv', "boy"];
 
     let keyword = check_in_sentence(string.toLowerCase(), tab)
-    
-    if (keyword === 'ping')
-        msg.reply('pong');
-    else if (keyword === 'eternium')
-        msg.reply('Work in progress');
-    else if (keyword === 'meme')
-        msg.reply('Work in progress');
-    else if (keyword === 'pierre')
-        msg.reply('Who DARES to speak about his esteemed Ass?');
-    else if (keyword === 'bite' || keyword === 'penis' || keyword == 'vagin' && msg.author.username != 'Chaika9') {
-        msg.reply('Get your mind out of the gutter PLEASE.');
-        msg.delete();
+    if (msg.author.username != 'GreetTextBot' && msg.channel.name == 'le-bot') {
+        if (keyword === 'ping')
+            msg.reply('pong');
+        else if (keyword === 'eternium')
+            msg.reply('Work in progress');
+        else if (keyword === 'meme')
+            msg.reply('Work in progress');
+        else if (keyword === 'pierre')
+            msg.reply('Who DARES to speak about his esteemed Ass?');
+        else if (keyword === 'meilleur serv')
+            msg.reply('MEILLEUR SERV');
+
+        //bad words
+        else if (keyword === 'bite' || keyword === 'penis' || keyword == 'vagin') {
+            msg.reply('Get your mind out of the gutter PLEASE.');
+            msg.delete();
+        }
+
+        //gifs reactions
+        else if (keyword === 'queen')
+            msg.reply('https://tenor.com/view/nia-xenoblade-chronicles2-nia-sawg-gif-18511161');
+        else if (keyword === 'boy')
+            msg.reply('https://tenor.com/view/kratos-boy-kratos-boy-boi-god-of-war-gif-12278930');
+        else if (keyword === 'mathis' || keyword === 'matis')  
+            msg.reply('https://tenor.com/view/anime-lucina-irritated-dance-peace-sign-gif-13933637');
+        else if (keyword === 'nice')
+            msg.reply('https://tenor.com/view/pootis-engage-yes-very-yes-heavy-tf2-gif-17755424');  
     }
-    else if (keyword === 'meilleur serv')
-        msg.reply('MEILLEUR SERV');
-    else if (keyword === 'boy' && msg.author.username != 'GreetTextBot')
-        msg.reply('https://tenor.com/view/kratos-boy-kratos-boy-boi-god-of-war-gif-12278930');
-    else if (keyword === 'mathis' || keyword === 'matis')  
-        msg.reply('https://tenor.com/view/anime-lucina-irritated-dance-peace-sign-gif-13933637');
 });
 
 
